4a84cc086243e574631ee345eee0e57906c5dec9
oahmad33898 <osama.ahmed@systemsltd.com>
2022-09-27 13:48:39 +0500

    commit message

---
+++ src/resumeData.js
@@ -0,0 +1,204 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Osama Ahmed",  
+    "role": "Software Engineer",
+    "linkedinId":"Osama-Ahmed",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/osama-ahmed-95189b154",
+          "className":"fa fa-linkedin"
+        },
+        // {
+        //   "name":"github",
+        //   "url":"http://github.com/rbhatia46",
+        //   "className":"fa fa-github"
+        // },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"Software Engineer having experience of more than 2 year on React, Node, Angular, React Native. Experience in working with RESTful Web Services and implementing RESTful API's. A highly motivated and dedicated developer ensuring high performance on web applications development and always eager to learn new technologies.",
+    "address":"Pakistan",
+    "website":"+923018177043",
+    "education":[
+      {
+        "UniversityName":"Bahria University",
+        "specialization":"Bachelors in Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Created a smart monitoring system for comatose patients for FYP"
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Systems Limited",
+        "specialization":"Junior Consultant",
+        "MonthOfLeaving":"2022-Present",
+        // "YearOfLeaving":"2018",
+        "Achievements":"Leading a backend team and interact with stakeholders throughout the product development cycle.Development of RESTful Web Services and implementing RESTful API's Responsible for development, configuration, deployments and maintenance of assigned tasks. Analyzing and designing new features in the application. Coordinated the product roadmap to improve quality, consistency, and client satisfaction"
+        
+      },
+      {
+        "CompanyName":"Systems Limited",
+        "specialization":"Associate Consultant",
+        "MonthOfLeaving":"2021-2022",
+        "Achievements":"Developing web applications in React-JS and Angular. Developing mobile applications in React-Native (IOS /Android) and Cross platform.Development of APIs in NodeJS. Responsible for development, configuration and maintenance of assigned tasks. Analyzing and designing new features in the Application. Collaborated in fixing memory leakages and performance issues in the application. "
+      }
+    ],
+    "projects":[
+      {
+        "CompanyName":"1link SDRP",
+        // "specialization":"Junior Consultant",
+        // "MonthOfLeaving":"2022-Present",
+        // "YearOfLeaving":"2018",
+        "Achievements":"Sohni Dharti Remittance Program (SDRP) is a point-based loyalty scheme for our remitters who work abroad and send money to their loved ones in Pakistan through banking channels or exchange companies.",
+        "responsibility":"Responsibilities/ Tasks Performed in this project:",
+        "activities":[
+          {
+            "name":"Leading a team of backend developers for the App and portal backend using Node-JS and Oracle"
+          },
+          {
+            "name":"Working on 1link portal using Angular"
+          },
+          {
+            "name":"Working on Rest APIs"
+          },
+          {
+            "name":"Development of RESTful Web Services and implementing RESTful APIs"
+          },
+          {
+            "name":"Interact with stakeholders throughout the product development cycle"
+          },
+          
+          {
+            "name":"Technologies: Node JS, Angular"
+          },
+          {
+            "name":"Development Tools: VS Code, SQL Developer"
+          }
+        ]
+        
+      },
+      {
+        "CompanyName":"eTIP-Health Wallet",
+        // "specialization":"Junior Consultant",
+        // "MonthOfLeaving":"2022-Present",
+        // "YearOfLeaving":"2018",
+        "Achievements":"t is a digital solution to the contemporary challenges that haverisen in the wake of the coronavirus pandemic. It provides a complete solution for automating the process of coronavirus testing for all kinds of users ranging from citizens to medical health workers.",
+        "responsibility":"Responsibilities/ Tasks Performed in this project:",
+        "activities":[
+          {
+            "name":"Worked on Mobile application using React-Native"
+          },
+          {
+            "name":"Worked on React, Redux, Saga, ES6+."
+          },
+          {
+            "name":"Worked on Rest APIs, React Hooks."
+          },
+          {
+            "name":"Worked on Bootstrap, Axios, Fetch."
+          },
+          
+          {
+            "name":"Technologies: React, Node JS"
+          },
+          {
+            "name":"Development Tools: VS Code"
+          }
+        ]
+        
+      },
+      {
+        "CompanyName":"Pre-flight COVID-19 Portal – Pure Health",
+        // "specialization":"Junior Consultant",
+        // "MonthOfLeaving":"2022-Present",
+        // "YearOfLeaving":"2018",
+        "Achievements":"Travelers from across the globe can now book a Covid-19 pretravel RT-PCR test through this application.",
+        "responsibility":"Responsibilities/ Tasks Performed in this project:",
+        "activities":[
+          {
+            "name":"Admin Portal – (For managing Labs, Reference data setup,users and their roles )"
+          },
+          {
+            "name":"Lab Portal - (For adding test results of the applicants)"
+          },
+          {
+            "name":"Applicant Portal – (From where customers can book there COVID-19 tests)"
+          },
+          {
+            "name":"Technologies: React, Node JS"
+          },
+          {
+            "name":"Development Tools: VS Code"
+          }
+        ]
+        
+      },
+      
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Node"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Angular"
+      },
+      {
+        "skillname":"React-Native"
+      },
+      {
+        "skillname":"Android"
+      },
+      
+    
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Osama is a great guy to work with. From project start to its deployment, he has a grip on everything. He knows fully well about what is needed to be done to complete the work. He is a stand up guy who takes care of his team and knows fully well on how to utilise their skills properly.l",
+        "name":"Shaiharyaar Ahmad"
+      },
+      {
+        "description":"I've known Osama for over 8+ years and in that time I've found him to be a person of the highest caliber. He lives by two things, Loyalty and hard work. In my personal and professional opinion, he'd be a great asset for any company that's lucky enough to hire him",
+        "name":"Majid Khan Burki"
+      },
+      {
+        "description":"Osama is goal-oriented, focused, and highly ambitious. His knowledge is vast and extensive, and he possesses exceptional leadership skills. He has commendable problem solving skills which is a cherry on top of his excellent development skills.",
+        "name":"Syed Muhammad Jaffer Abbas"
+      }
+    ]
+  }
+  
+  export default resumeData
